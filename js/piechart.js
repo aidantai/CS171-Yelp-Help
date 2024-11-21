2acde6075cf8d29eab3d20c69c136bdb8a39ae2b
hchang00 <90081365+hchang00@users.noreply.github.com>
2024-11-20 21:46:47 -0500

    Updated the first couple pages in index.html

--- js/piechart.js
+++ js/piechart.js
@@ -16,8 +16,8 @@ class PieReviews {
         let vis = this;
 
         vis.margin = {top: 40, right: 40, bottom: 40, left: 40};
-        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width*0.8 - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height*0.8 - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
